d039110fe1fa032ed6c6a52c62692e0fe3f38752
krauserhuang <crescentdence@gmail.com>
2022-03-11 15:04:56 +0800

    Develop in Swift Data Collection Ch1.5 UITableViews

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Tai Chin Huang on 2022/3/10.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
